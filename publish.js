1c6230042e67237b93d6fd673f556bf2c06691b8
Matthew Wilkes <tonicblue@gmail.com>
2018-11-01 13:34:10 +0000

    Updated webpack stuff

--- publish.js
+++ publish.js
@@ -1,10 +1,23 @@
 const globby = require('globby');
+const fs = require('fs');
+const path = require('path');
 const mime = require('mime-types');
 const {Storage} = require('@google-cloud/storage');
 const admin = require('firebase-admin');
+const isGzip = require('is-gzip');
+const stringify = require('json-stringify-safe');
+const pako = require('pako');
 
 const package = require('./package.json');
+
 const firebaseServiceAccount = require('./service-accounts/firebase.json');
+
+const FIREBASE_ROOT = '/contexts-new/';
+const BUILD_DIR = path.join(__dirname, 'build');
+const BUCKET = 'scvo-assets';
+const DESTINATION_DIR = 'test';
+const VERSION = package.version;
+const VERSION_TEST = new RegExp('-' + VERSION.replace(/\./g, '\\.') + '\.[0-9a-z]+$', 'gi');
 
 const storage = new Storage({
   projectId: 'scvo-net'
@@ -16,59 +29,121 @@ const app = admin.initializeApp({
   credential: admin.credential.cert(firebaseServiceAccount)
 });
 
-const versionNo = package.version;
-const sites = ['digitalparticipation', 'emailer', 'fundingscotland', 'volunteerscotland-search', 'getinvolved', 'getinvolved-legacy', 'goodmoves', 'humanrightsdeclaration', 'scotlandforeurope', 'scvo'];
+(async () => {
+  const sites = fs.readdirSync(BUILD_DIR).filter(source => fs.lstatSync(path.join(BUILD_DIR, source)).isDirectory());
+  console.log('Publishing the following sites:', sites.join(', '));
+  await uploadAssets(sites);
+  await uploadConfigs(sites);
+})();
 
-async function main() {
-  const paths = await globby('build/**/*.gz');
-  for (const path of paths) {
-    const newPath = getNewPath(path);
-    const contentType = mime.lookup(newPath);
-    await uploadFile(path, newPath, contentType);
-  }
-  await updateSitesMetadata();
-}
+async function uploadAssets(sites) {
+  const uploadOptions = getUploadOptions(sites);
+  const total = uploadOptions.length;
+  let successful = 0;
+  let failed = 0;
+  
+  console.log('Uploading a grand total of', total, 'assets');
 
-function getNewPath(path) {
-  const parts = path.split('.');
-  const ext = parts.splice(parts.length - 2)[0];
-  const name = parts.join('.').replace(/^build\//, '');
-  const newName = name + '-' + versionNo + '.' + ext;
-  return newName;
-}
+  for (const uploadOption of uploadOptions) {
+    let message = '';
+    const extraInfo = [];
+    
+    if (uploadOption.versioned) {
+      extraInfo.push('versioned');
+    }
 
-async function uploadFile(path, newPath, contentType) {
-  try {
-    await storage.bucket('scvo-assets').upload(path, {
-      destination: newPath,
-      public: true,
-      metadata: {
-        cacheControl: 'public, max-age=31536000',
-        contentType: contentType,
-        contentEncoding: 'gzip'
-      }
-    });
-    console.log('Uploaded', path, 'to scvo-assets ->', newPath);
-  } catch(err) {
-    console.error('Failed to upload', path, 'to scvo-assets ->', newPath, err);
-  }
-}
+    if (uploadOption.gzipped) {
+      extraInfo.push('gzipped');
+    }
 
-async function updateSitesMetadata() {
-  for (const site of sites) {
-    const path = '/contexts/' + site + '/metaData/assetsVersion/'
+    if (extraInfo.length === 0) {
+      extraInfo.push('boring');
+    }
+
     try {
-      await app.database().ref(path).set(versionNo);
-      console.log('Updated site metadata for', site, 'at path', path);
+      await storage.bucket(BUCKET).upload(uploadOption.path, uploadOption.options);
+      successful++;
+      message = 'SUCCESS -> ' + uploadOption.options.destination;
     } catch(err) {
-      console.error('Failed to update site metadata for', site, 'at path', path);
+      failed++;
+      message = 'FAILED -> ' + uploadOption.options.destination + ':\n' + stringify(err, null, 2);
+    }
+
+    const prefix = ['[F:', failed, '+ S:', successful, '=', (failed + successful), '/', total + ']'].join(' ');
+    console.log(prefix, '(' + extraInfo.join(', ') + ')',  message);
+  }
+
+  console.log('Finished uploading', successful, 'successful assets with', failed, 'failures');
+}
+
+function getUploadOptions(sites) {
+  const uploadOptions = [];
+
+  for (const site of sites) {
+    const siteDir = path.join(BUILD_DIR, site);
+    const configPath = path.join(siteDir, site + '-site.json');
+    const assetsGlob = path.join(siteDir, '**/*');
+    const assets = globby.sync([assetsGlob, '!' + configPath]);
+
+    for (const asset of assets) {
+      try {
+        const destination = path.join(DESTINATION_DIR, site, asset.split(siteDir)[1]);
+        const contentType = mime.lookup(asset) || 'text/plain';
+        const versioned = VERSION_TEST.test(asset);
+        const gzipped = isGzip(fs.readFileSync(asset));
+
+        const options = {
+          destination,
+          public: true,
+          metaData: {
+            contentType
+          }
+        };
+
+        if (!!versioned) {
+          options.metaData.cacheControl = 'public, max-age=31536000';
+        }
+
+        if (gzipped) {
+          options.metaData.contentEncoding = 'gzip';
+        }
+
+        uploadOptions.push({ path: asset, versioned, gzipped, options });
+      } catch(err) {
+        console.error('Failed to get options for asset:', asset, err);
+      }
     }
   }
+
+  return uploadOptions;
 }
 
-main().then(() => {
-  console.log('Program completed');
-  process.exit();
-}).catch(err => {
-  console.error('Program failed:', err);
-});
+async function uploadConfigs(sites) {
+  const total = sites.length;
+  let successful = 0;
+  let failed = 0;
+
+  console.log('Uploading site configurations for', total, 'sites');
+
+  for (const site of sites) {
+    const firebasePath = FIREBASE_ROOT + site;
+    const configPath = path.join(BUILD_DIR, site, site + '-site.json');
+    let message = '';
+
+    try {
+      const json = fs.readFileSync(configPath).toString();
+      const siteConfig = JSON.parse(json);
+      await app.database().ref(firebasePath).set(siteConfig);
+      successful++;
+      message = 'SUCCESS -> ' + firebasePath;
+    } catch(err) {
+      failed++;
+      message = 'FAILED -> ' + firebasePath + ':\n' + stringify(err, null, 2);
+    }
+    
+    const prefix = ['[F:', failed, '+ S:', successful, '=', (failed + successful), '/', total + ']'].join(' ');
+    console.log(prefix, message);
+  }
+
+  console.log('Finished successfully uploading', successful, 'site configurations with', failed, 'failures');
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,132 +1,24 @@
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
-const autoprefixer = require('autoprefixer');
-const CompressionPlugin = require('compression-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const globby = require('globby');
+const path = require('path');
 
-const sites = [
-  { site: 'digitalparticipation', library: 'DigitalParticipation' },
-  { site: 'fundingscotland', library: 'FundingScotland' },
-  { site: 'getinvolved', library: 'GetInvolved' },
-  { site: 'volunteerscotland-search', library: 'VolunteerScotlandSearch' },
-  { site: 'goodmoves', library: 'Goodmoves' },
-  { site: 'humanrightsdeclaration', library: 'HumanRightsDeclaration' },
-  { site: 'scotlandforeurope', library: 'ScotlandForEurope' },
-  { site: 'scvo', library: 'SCVO' }
-]
+const toInclude = process.env.SITES && process.env.SITES.split(',') || null;
 
-function main() {
-  let toCompile = [];
-  if (process.env.SITES) {
-    const names = process.env.SITES.split(',');
-    sites.forEach(site => {
-      if (names.indexOf(site.name) > -1) {
-        toCompile.push(site);
-      }
-    });
-  }
-  toCompile = toCompile.length > 0 ? toCompile : sites;
-  const noCompression = !!process.env.NO_COMPRESS;
-
-  console.log('Packing sites:', toCompile);
-
-  const plugins = [
-    new HardSourceWebpackPlugin()
-  ];
-
-  console.log('No Compression:', noCompression);
-  if (!noCompression) {
-
-    plugins.push(new UglifyJsPlugin({
-      uglifyOptions: {
-        ecma: 5,
-        sourceMap: true
-      }
-    }));
-
-    plugins.push(new CompressionPlugin());
-    console.log('Added compression plugins ',plugins);
-  }
-
-  const modules = toCompile.map(site => {
-    const config = getConfig(site.site, site.library, plugins);
-    return config;
-  });
-
-  return modules;
+if (toInclude) {
+  console.log('Only building the following sites:', toInclude);
 }
 
-function getConfig(site, library, plugins) {
-  const config = {
-    entry: ['./sites/' + site + '/main.scss', './sites/' + site + '/main.js'],
-    output: {
-      filename: 'build/' + site + '/main.js',
-      library: library,
-      libraryTarget: 'var'
-    },
-    module: {
-      rules: [{
-          test: /\.scss$/,
-          use: [{
-              loader: 'file-loader',
-              options: {
-                name: 'build/' + site + '/main.css',
-              },
-            },
-            {
-              loader: 'extract-loader'
-            },
-            {
-              loader: 'css-loader'
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: () => [autoprefixer()],
-              },
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                includePaths: ['./node_modules'],
-              },
-            }
-          ],
-        },
-        {
-          test: require.resolve('jquery'),
-          use: [
-            {
-              loader: 'expose-loader',
-              options: 'jQuery'
-            },
-            {
-              loader: 'expose-loader',
-              options: '$'
-            }
-          ]
-        },
-        {
-          test: require.resolve('string'),
-          use: [
-            {
-              loader: 'expose-loader',
-              options: 'S'
-            }
-          ]
-        },
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          query: {
-            presets: ['es2015'],
-          },
-        }
-      ],
-    },
-    plugins: plugins
-  };
+const siteConfigPaths = globby.sync('sites/**/webpack.config.js');
+const configs = [];
 
-  return config;
-}
+for (const configPath of siteConfigPaths) {
+  const modulePath = './' + configPath.replace(/\.js$/, '');
+  const modulePathParts = path.dirname(modulePath).split('/');
+  const siteName = modulePathParts[modulePathParts.length - 1];
+  if (!toInclude || ~toInclude.indexOf(siteName)) {
+    console.log(siteName, '-> Adding', modulePath, 'to webpack build');
+    const config = require(modulePath);
+    configs.push(config);
+  }
+};
 
-module.exports = main();
+module.exports = configs;
